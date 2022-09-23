31c1fb670414e7189a9156d11fd003fc9628fe78
andrea <andreajacome2509@gmail.com>
2022-09-23 18:28:28 +0200

    ejercicio 06 finalizado (falta poner monospace para que visualmente quede estructurado)

---
+++ EjerciciosPHP02/06.php
@@ -0,0 +1,23 @@
+<?php
+$min=1;
+$max=10;
+$num=rand($min,$max);
+
+echo "Número generado: ".$num."<br>";
+for ($i=1; $i<=$num;$i++) {
+    echo "**** ";
+}
+echo "<br>";
+for ($i=1; $i<=$num;$i++) {
+    echo "**** ";
+}
+echo "<br>";
+if ($num==1) {
+    echo "****";
+} else {
+    for ($i=0; $i<$num;$i++) {
+        echo "*****";
+    }
+}
+
+
