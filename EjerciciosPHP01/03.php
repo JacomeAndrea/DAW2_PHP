e704fd2af2bd6518557834aa5093a00c9de256cb
andrea <andreajacome2509@gmail.com>
2022-09-26 12:13:09 +0200

    monospace funcionable (se ha añadido &nbsp)

--- EjerciciosPHP01/03.php
+++ EjerciciosPHP01/03.php
@@ -1,12 +1,20 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+<h1>Ejercicio 3</h1>
+<code>
 <?php
 $min = 1;
 $max = 9;
 $random = rand($min, $max);
 
-function piramideAsteriscos($random) {
+    function piramideAsteriscos($random) {
     for ($i = 0; $i <= $random; $i++) {
         for ($j=1;$j<=$random-$i;$j++) {
-            echo" ";
+            echo"&nbsp";
         }
         for ($j=1; $j<=2*$i-1;$j++) {
             echo "*";
@@ -19,4 +27,12 @@ function piramideAsteriscos($random) {
 echo "PIRAMIDE DE ASTERICOS"."<br>";
 echo "<br>"."Número aleatorio: ".$random."<br>";
 echo "<br>";
-echo "<style fontFamily='monospace' > " . piramideAsteriscos($random) ."</style>";
+echo piramideAsteriscos($random);
+?>
+
+
+</code>
+
+</body>
+</html>
+
