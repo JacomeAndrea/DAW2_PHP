11372d0169ed26d3c1bd4d7a6bab0d8dd97a3170
andrea <andreajacome2509@gmail.com>
2022-12-12 20:51:55 +0100

    CRUD con paginación

--- 9-bases-de-datos/CRUD/app/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conexion = new mysqli ("localhost", "root", "root", "CRUD_DB");
-
-if ($conexion->connect_errno) {
-    // Comprueba conexión
-    printf("Conexión fallida: %s\n", mysqli_connect_error());
-    exit();
-}
